4b94ebedf5c7cc06245c912c4e0154ba6ecfb400
LDFOUR\luisd <luisd@jazusoft.com>
2025-02-18 18:24:37 -0300

    changed validation

--- app/Models/Contributor.php
+++ app/Models/Contributor.php
@@ -39,7 +39,7 @@ class Contributor extends Model
     protected $rules = [
         'name' => 'required|string|between:2,100',
         'logo' => 'string|between:2,100',
-        'org_id' => 'required|integer',
+        'org_detail_id' => 'required|integer',
     ];
 
 
