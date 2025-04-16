774ac1976ff0085210b3705508e06fb8c42feb09
LDFOUR\luisd <luisd@jazusoft.com>
2025-04-16 16:48:53 -0300

    resolved migration logic.

--- database/migrations/2025_03_25_127530_change_region_to_subnational.php
+++ database/migrations/2025_03_25_127530_change_region_to_subnational.php
@@ -6,28 +6,33 @@ use Illuminate\Support\Facades\Schema;
 
 class ChangeRegionToSubnational extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
+        Schema::table('usage_logs', function (Blueprint $table) {
+            $table->string('subnational')->nullable();
+        });
+    
         DB::table('usage_logs')->update([
             'subnational' => DB::raw('region')
         ]);
+    
+        Schema::table('usage_logs', function (Blueprint $table) {
+            $table->dropColumn('region');
+        });
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    
     public function down()
-    {
+{
+    Schema::table('usage_logs', function (Blueprint $table) {
+        $table->string('region')->nullable();
+    });
 
-        DB::table('usage_logs')->update([
-            'region' => DB::raw('subnational')
-        ]);
-    }
+    DB::table('usage_logs')->update([
+        'region' => DB::raw('subnational')
+    ]);
+
+    Schema::table('usage_logs', function (Blueprint $table) {
+        $table->dropColumn('subnational');
+    });
+}
 }
